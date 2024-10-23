9938d4383569a1c99f58b462e552dca214f95e65
Justin Crowe <Justin.Crowe@btl.com>
2024-10-23 16:58:35 +0100

    Make sure the difficulty values are 5 decimal places to avoid issues in csv parsing logic in Surpass
    Add a 5 second delay between calls to open-trivia to avoid the heavy rate limiting
    
    Removed a gac reference.. i dont know why thats there...

--- src/SurpassDataPopulator.Domain/Data/Builders/ItemTags/Adaptive/AdaptiveTagRequirements.cs
+++ src/SurpassDataPopulator.Domain/Data/Builders/ItemTags/Adaptive/AdaptiveTagRequirements.cs
@@ -30,23 +30,16 @@ namespace SurpassDataPopulator.Domain.Data.Builders.ItemTags.Adaptive
         {
             return item.Difficulty switch
             {
-                Difficulty.Easy => (ThreadSafeRandom.ThisThreadsRandom.Next(0, 3) + GetRandomDouble()).ToString(CultureInfo.InvariantCulture),
-                Difficulty.Medium => (ThreadSafeRandom.ThisThreadsRandom.Next(3, 7) + GetRandomDouble()).ToString(CultureInfo.InvariantCulture),
-                Difficulty.Hard => (ThreadSafeRandom.ThisThreadsRandom.Next(7, 10) + GetRandomDouble()).ToString(CultureInfo.InvariantCulture),
+                Difficulty.Easy => (ThreadSafeRandom.ThisThreadsRandom.Next(0, 3) + double.Parse(GetRandomDouble())).ToString(CultureInfo.InvariantCulture),
+                Difficulty.Medium => (ThreadSafeRandom.ThisThreadsRandom.Next(3, 7) + double.Parse(GetRandomDouble())).ToString(CultureInfo.InvariantCulture),
+                Difficulty.Hard => (ThreadSafeRandom.ThisThreadsRandom.Next(7, 10) + double.Parse(GetRandomDouble())).ToString(CultureInfo.InvariantCulture),
                 _ => throw new ArgumentOutOfRangeException(nameof(item.Difficulty))
             };
         }
 
-        private static double GetRandomDouble()
+        private static string GetRandomDouble()
         {
-            return SetSigFigs(ThreadSafeRandom.ThisThreadsRandom.NextDouble(), 5);
-        }
-
-        private static double SetSigFigs(double value, int digits)
-        {   
-            if(value == 0) return 0;
-            var scale = (decimal)Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(value))) + 1);
-            return (double) (scale * Math.Round((decimal)value / scale, digits, MidpointRounding.AwayFromZero));
+            return ThreadSafeRandom.ThisThreadsRandom.NextDouble().ToString("F5", CultureInfo.InvariantCulture);
         }
     }
 }
--- src/SurpassDataPopulator.Infrastructure/Services/OpenTriviaApi/OpenTriviaApiService.cs
+++ src/SurpassDataPopulator.Infrastructure/Services/OpenTriviaApi/OpenTriviaApiService.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using SurpassDataPopulator.Application.Interfaces.Services;
 using SurpassDataPopulator.Domain.Data.Builders.Items;
+using SurpassDataPopulator.Infrastructure.Http;
 using SurpassDataPopulator.Infrastructure.Services.OpenTriviaApi.Dto;
 using SurpassDataPopulator.Infrastructure.Services.OpenTriviaApi.Mapping;
 
@@ -12,10 +14,12 @@ namespace SurpassDataPopulator.Infrastructure.Services.OpenTriviaApi
     public class OpenTriviaApiService : IItemGeneratorService
     {
         private readonly IOpenTriviaApi _openTriviaApi;
-        
-        public OpenTriviaApiService(IOpenTriviaApi openTriviaApi)
+        private readonly ILogger<OpenTriviaApiService> _logger;
+
+        public OpenTriviaApiService(IOpenTriviaApi openTriviaApi, ILogger<OpenTriviaApiService> logger)
         {
             _openTriviaApi = openTriviaApi;
+            _logger = logger;
         }
         
         public async Task<ItemBuildResult> GenerateItemsAsync(ItemBuildRequest request)
@@ -39,6 +43,13 @@ namespace SurpassDataPopulator.Infrastructure.Services.OpenTriviaApi
             const int maxNumberOfQuestions = 50;
             var requestsRequired = (numberOfItemsPerCategory - 1) / maxNumberOfQuestions + 1;
             
+            const int waitBetweenCalls = 5000;
+
+            if (requestsRequired > 1)
+            {
+                _logger.LogInformation($"Calls to open-trivia are heavily rate limited, so we will wait {waitBetweenCalls / 1000} seconds between calls to avoid 429s. It may take longer to generate the items.");
+            }
+            
             foreach (var category in categories)
             {
                 for (var i = 0; i < requestsRequired; i++)
@@ -51,6 +62,8 @@ namespace SurpassDataPopulator.Infrastructure.Services.OpenTriviaApi
                     
                     if (response.ResponseCode != 0)
                         throw new Exception($"Open trivia api error, response code was {response.ResponseCode}");
+
+                    await Task.Delay(waitBetweenCalls);
 
                     openTriviaResponses.Add(response);
                 }
